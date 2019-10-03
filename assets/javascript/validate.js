9015650c6ea7097398ed4cb76009690c1c421c2f
Haakasaurus <kramerhjohnson@gmail.com>
2019-10-03 14:18:36 -0700

    Created validate.js to validate input from user, completed string and time validations.

---
+++ assets/javascript/validate.js
@@ -0,0 +1,13 @@
+function stringValidation(str) {
+    let alpha = /^[A-Z -]+$/i;
+    return alpha.test(str) && str.length < 30;
+}
+
+function timeValidation(str) {
+    let reg = /^[0-9:]+$/,
+        hour = str.slice(0, 2),
+        min = str.slice(3);
+    console.log(hour);
+    console.log(min)
+    return reg.test(str) && str.length == 5 && parseInt(hour) < 24 && parseInt(min) < 60;
+}
