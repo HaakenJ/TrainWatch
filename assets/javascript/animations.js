af1c0137e2fe7a44b1e9fc6e428b56bacbdf139d
Haakasaurus <kramerhjohnson@gmail.com>
2019-10-09 12:19:58 -0700

    Got login page functioning, moved animations inside an .onAuthStateChanged event.

--- assets/javascript/animations.js
+++ assets/javascript/animations.js
@@ -1,26 +1,30 @@
-var morph1 = anime({
-    targets: '.polymorph#morph1',
-    points: [{
-            value: '1920,1080 0,1080 0,0 432.59,0 701,968'
-        },
-        {
-            value: '1920,1080 0,1080 0,0 65,0 701,968'
-        }
-    ],
-    easing: 'easeOutQuad',
-    duration: 2000,
-    loop: false
-});
-var morph2 = anime({
-    targets: '.polymorph#morph2',
-    points: [{
-            value: '1920,1080 0,1080 0,1080 1296,900 1920,1080'
-        },
-        {
-            value: '1920,1080 0,1080 0,708 1296,900 1920,626'
-        }
-    ],
-    easing: 'easeOutQuad',
-    duration: 2000,
-    loop: false
-});
+auth.onAuthStateChanged((user) => {
+    if (!user) {
+        var morph1 = anime({
+            targets: '.polymorph#morph1',
+            points: [{
+                    value: '1920,1080 0,1080 0,0 432.59,0 701,968'
+                },
+                {
+                    value: '1920,1080 0,1080 0,0 65,0 701,968'
+                }
+            ],
+            easing: 'easeOutQuad',
+            duration: 2000,
+            loop: false
+        });
+        var morph2 = anime({
+            targets: '.polymorph#morph2',
+            points: [{
+                    value: '1920,1080 0,1080 0,1080 1296,900 1920,1080'
+                },
+                {
+                    value: '1920,1080 0,1080 0,708 1296,900 1920,626'
+                }
+            ],
+            easing: 'easeOutQuad',
+            duration: 2000,
+            loop: false
+        });
+    }
+})
