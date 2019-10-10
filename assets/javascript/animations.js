099b1eb85e2357d5e6204eb85315142bc79f093e
Haakasaurus <kramerhjohnson@gmail.com>
2019-10-09 18:41:17 -0700

    Responsiveness kind of works, the shapes fly from the top left before animation starts though.

--- assets/javascript/animations.js
+++ assets/javascript/animations.js
@@ -1,12 +1,27 @@
+var viewHeight = window.innerHeight,
+    viewWidth = window.innerWidth,
+    svgOneStart = `${viewWidth},${viewHeight} 0,${viewHeight} 0,0 ${viewWidth * 0.2253},0 ${viewWidth},0`,
+    svgTwoStart = `${viewWidth},${viewHeight} 0,${viewHeight} 0,${viewHeight} ${viewWidth * 0.6531},${viewHeight} ${viewWidth},${viewHeight}`,
+    svgVB = `0 0 ${viewWidth} ${viewHeight}`;
+
+$(document).ready(() => {
+    $('#svg-one').attr('viewBox', svgVB);
+    $('#svg-two').attr('viewBox', svgVB);
+    $('#morph1').attr('points', svgOneStart);
+    $('#morph1').attr('points', svgTwoStart);
+})
+
+
+
 auth.onAuthStateChanged((user) => {
     if (!user) {
         var morph1 = anime({
             targets: '.polymorph#morph1',
             points: [{
-                    value: '1920,1080 0,1080 0,0 432.59,0 701,968'
+                    value: `${viewWidth},${viewHeight} 0,${viewHeight} 0,0 ${viewWidth * 0.2253},0 ${viewWidth * 0.3651},${viewWidth * 0.5042}`
                 },
                 {
-                    value: '1920,1080 0,1080 0,0 65,0 701,968'
+                    value: `${viewWidth},${viewHeight} 0,${viewHeight} 0,0 ${viewWidth * 0.0339},0 ${viewWidth * 0.3651},${viewWidth * 0.5042}`
                 }
             ],
             easing: 'easeOutQuad',
@@ -16,10 +31,10 @@ auth.onAuthStateChanged((user) => {
         var morph2 = anime({
             targets: '.polymorph#morph2',
             points: [{
-                    value: '1920,1080 0,1080 0,1080 1296,900 1920,1080'
+                    value: `${viewWidth},${viewHeight} 0,${viewHeight} 0,${viewHeight} ${viewWidth * 0.675},${viewWidth * 0.4688} ${viewWidth},${viewHeight}`
                 },
                 {
-                    value: '1920,1080 0,1080 0,708 1296,900 1920,626'
+                    value: `${viewWidth},${viewHeight} 0,${viewHeight} 0,${viewWidth * 0.3688} ${viewWidth * 0.675},${viewWidth * 0.4688} ${viewWidth},${viewWidth * 0.3260}`
                 }
             ],
             easing: 'easeOutQuad',
